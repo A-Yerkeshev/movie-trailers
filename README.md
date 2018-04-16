0b8abccad5c65065cdcc576518fb6778bea3eb33
Arman Yerkeshev <a.yerkesh@gmail.com>
2018-04-16 17:52:49 +0600

    Essential files set

---
+++ README.md
@@ -0,0 +1,21 @@
+# The movies trailers webpage.
+
+### Requested software
+In order to successfully run this application on the local computer you need to have Python installed.
+
+### Installation
+
+If you do not have a Python installed on your computer follow this steps:
+1. Go to www.python.org.
+2. Download Python2.X or Python3.X version for your operational system.
+3. Install it on local computer, following instructions on Python official website.
+
+### Running the application
+	
+Once you have Python installed follow this steps:
+1. Open Python IDLE.
+2. In shell open a movies.py file.
+3. From top menu select 'Run', then 'Run Module' or simply press 'F5'.
+	
+You also can run the file from command line using command 'python' followed by whitespace 
+and name of the file.
---
+++ movies.py
@@ -0,0 +1,40 @@
+import media
+import fresh_tomatoes
+
+# Populate the template with data
+taxi = media.Movie("Taxi",
+                   '''To work off his tarnished driving record, a hip taxi
+                   driver must chauffeur a loser police inspector on the
+                   trail of German bank robbers.''',
+                   '''img/taxi1.jpg''',
+                   "https://www.youtube.com/watch?v=BBnlCXi2WWA")
+taxi_2 = media.Movie("Taxi 2",
+                     '''Police inspector Emilien and his taxi-driver pal
+                     Daniel are back, this time on the tail of a group of
+                     Japanese yakuza.''',
+                     '''img/taxi2.png''',
+                     "https://www.youtube.com/watch?v=XkMZ75BwhCg")
+taxi_3 = media.Movie("Taxi 3",
+                     '''Out to stop a new gang disguised as Santa Claus, Emilien
+                     and Daniel must also handle major changes in their
+                     personal relationships.''',
+                     '''img/taxi3.jpg''',
+                     "https://www.youtube.com/watch?v=XLP-rf3dHOQ")
+taxi_4 = media.Movie("Taxi 4",
+                     '''Unlucky, clumsy, charming Marseile PD detective Emilien
+                     Coutant-Kerbalec must assist worse-than-Clouseau
+                     commissioner Gibert guarding a Belgian criminal reputed as
+                     dangerous as - and caged like Hannibal Lector.''',
+                     '''img/taxi4.jpg''',
+                     "https://www.youtube.com/watch?v=FVNzRNZDllI")
+
+# Open the page in browser
+fresh_tomatoes.open_movies_page([taxi, taxi_2, taxi_3, taxi_4])
+
+#Authors attribution
+#https://en.wikipedia.org/w/index.php?curid=24751162
+#By Source, Fair use, https://en.wikipedia.org/w/index.php?curid=1124037
+#https://en.wikipedia.org/w/index.php?curid=4842798
+#By Source, Fair use, https://en.wikipedia.org/w/index.php?curid=37030442
+
+
