5d253fef3443cde88a0ae330828d31a17a004c4b
Arman Yerkeshev <a.yerkesh@gmail.com>
2018-04-19 08:28:08 +0600

    Add files to run on Heroku

--- media.py
+++ media.py
@@ -1,6 +1,6 @@
 import webbrowser
 
-
+# Set class for movie objects
 class Movie():
     def __init__(
             self, movie_title, movie_storyline, poster_image, trailer_youtube):
@@ -9,6 +9,7 @@ class Movie():
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
+# Property to open trailer when clicked
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
 
